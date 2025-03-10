28643e7f32d7517f137687bcc6568ae3cf428ca5
gretafg <gretafg@al.insper.edu.br>
2025-03-10 16:06:36 -0300

    exercicio 2

--- exe2/main.c
+++ exe2/main.c
@@ -11,6 +11,12 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile int flag_led_r = 0;
+volatile int flag_led_g = 0;
+
+volatile int g_timer_g = 0;
+volatile int g_timer_r = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
@@ -20,12 +26,26 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_g_callback(repeating_timer_t *rt){
+    g_timer_g = 1;
+    flag_led_g = 1;
+    return true;
+}
+
+bool timer_r_callback(repeating_timer_t *rt){
+    g_timer_r = 1;
+    flag_led_r = 1;
+    return true;
+}
+
 int main() {
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R,0);
 
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_put(LED_PIN_G,0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -39,14 +59,56 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
-    while (true) {
+    repeating_timer_t timer_g;
+    repeating_timer_t timer_r;
 
-        if (flag_r) {
-            flag_r = 0;
+    while (true) {
+        if (flag_led_g){
+            if(gpio_get(LED_PIN_G)){
+                gpio_put(LED_PIN_G,0);
+            } else{
+                gpio_put(LED_PIN_G,1);
+            }
+            flag_led_g = 0;
+        }
+        if (flag_led_r){
+            if(gpio_get(LED_PIN_R)){
+                gpio_put(LED_PIN_R,0);
+            } else{
+                gpio_put(LED_PIN_R,1);
+            }
+            flag_led_r =0;
         }
 
         if (flag_g) {
+            if (!g_timer_g){
+                if (add_repeating_timer_ms(250, 
+                    timer_g_callback,
+                    NULL, 
+                    &timer_g)) {
+                        g_timer_g = 1;
+                }
+            } else {
+                cancel_repeating_timer(&timer_g);
+                g_timer_g = 0;
+                gpio_put(LED_PIN_G, 0);
+            }
             flag_g = 0;
+        }
+        if (flag_r) {
+            if (!g_timer_r){
+                if (add_repeating_timer_ms(500, 
+                    timer_r_callback,
+                    NULL, 
+                    &timer_r)) {
+                        g_timer_r = 1;
+                }
+            } else {
+                cancel_repeating_timer(&timer_r);
+                g_timer_r = 0;
+                gpio_put(LED_PIN_R, 0);
+            }
+            flag_r = 0;
         }
     }
 }
